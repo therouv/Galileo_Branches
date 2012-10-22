9552e831cfb3b3ae66c33c1615948ba7b95f7f1a
Rouven Alexander Rieker <therouv@googlemail.com>
2012-10-22 11:26:16 +0200

    Backend Controller erstellt

---
+++ src/app/code/community/Galileo/Branches/controllers/Adminhtml/Branches.php
@@ -0,0 +1,10 @@
+<?php
+
+class Galileo_Branches_Adminhtml_BranchesController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
