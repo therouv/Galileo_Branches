6046b652c3177d0249463d3a9bd8573968cce22c
Rouven Alexander Rieker <therouv@googlemail.com>
2012-10-22 13:12:58 +0200

    Models

--- src/app/code/community/Galileo/Branches/Model/Resource/Branch.php
+++ src/app/code/community/Galileo/Branches/Model/Resource/Branch.php
@@ -4,6 +4,6 @@ class Galileo_Branches_Model_Resource_Branch extends Mage_Core_Model_Resource_Db
 {
     protected function _construct()
     {
-        $this->_init('branches/branch', 'id');
+        $this->_init('branches/branches', 'id');
     }
 }
